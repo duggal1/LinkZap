33152a567bfd5fcef8e4dc85a84d0b8afa55597c
harshit duggal <harshitduggal56@gmail.com>
2025-03-27 17:40:54 +0530

    🔄 Updated codebase

--- src/components/landing/urls/try-linkzap.tsx
+++ src/components/landing/urls/try-linkzap.tsx
@@ -1,11 +1,11 @@
 import { UrlShortenerForm } from "@/components/urls/url-shortener-form";
-
+//Updated Header Cta
 const TryLinkzaps = () => {
   return (
     <div className="flex flex-1 flex-col items-center justify-center min-h-screen bg-transparent backdrop-blur-sm p-6 md:p-24">
       <div className="w-full max-w-3xl mx-auto text-center space-y-8">
         <div className="space-y-4">
-          <h1 className="text-5xl md:text-6xl font-extrabold tracking-tighter bg-gradient-to-r from-orange-600 to-yellow-600 bg-clip-text text-transparent animate-gradient">
+          <h1 className="text-5xl md:text-6xl font-extrabold tracking-tighter bg-gradient-to-r from-white to-neutral-200 bg-clip-text text-transparent animate-gradient">
             Shorten Your Links
           </h1>
           <p className="text-lg text-gray-300/80 font-light max-w-2xl mx-auto leading-relaxed">
